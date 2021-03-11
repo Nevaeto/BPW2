64be7e52e7c6089fd7df9910ee7054745b1cefe1
Nevaeto <maaike.smidt@live.nl>
2021-03-11 12:06:14 +0100

    BPW Assets toegevoegd

---
+++ BPW 2D GAME RENDER PIPELINE/Assets/SpriteManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SpriteManager : MonoBehaviour
+{
+    public GameObject[] daySprites;
+    public GameObject[] nightSprites;
+
+    public PostProcessProfile day;
+    public PostProcessProfile night;
+
+    public PostProcessVolume ppv;
+    public bool dayStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dayStart)
+        {
+            ppv.profile = day;
+            foreach (GameObject go1 in daySprites)
+            {
+                go1.SetActive(true);
+            }
+            foreach (GameObject go2 in nightSprites)
+            {
+                go2.SetActive(false);
+            }
+        }
+        else
+        {
+            ppv.profile = night;
+            foreach (GameObject go1 in daySprites)
+            {
+                go1.SetActive(false);
+            }
+            foreach (GameObject go2 in nightSprites)
+            {
+                go2.SetActive(true);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    public void changeDay()
+    {
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //    if (ppv.profile == day)
+        //    {
+        //        ppv.profile = night;
+        //    }
+        //    else
+        //    {
+        //        ppv.profile = day;
+        //    }
+        //}
+
+        if (ppv.profile == day)
+        {
+            ppv.profile = night;
+
+            foreach (GameObject go1 in daySprites)
+            {
+                go1.SetActive(false);
+            }
+            foreach (GameObject go2 in nightSprites)
+            {
+                go2.SetActive(true);
+            }
+        }
+
+        if (ppv.profile == night)
+        {
+            ppv.profile = day;
+            foreach (GameObject go1 in daySprites)
+            {
+                go1.SetActive(true);
+            }
+            foreach (GameObject go2 in nightSprites)
+            {
+                go2.SetActive(false);
+            }
+        }
+    }
+}
---
+++ BPW 2D GAME RENDER PIPELINE/Assets/SwitchPostProcess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SwitchPostProcess : MonoBehaviour
+{
+    public SpriteManager sm;
+    public GameObject triggerPoint;
+
+    void Update()
+    {
+        //Debug.Log(Vector2.Distance(this.gameObject.transform.position, triggerPoint.transform.position));
+        if(Vector2.Distance(this.gameObject.transform.position, triggerPoint.transform.position) < 3.5f && Input.GetKeyDown(KeyCode.R)){
+            sm.changeDay();
+            Debug.Log("yep");
+        }
+    }
+}
